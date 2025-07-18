9cd297b86d39f98d6f0f70abf375ebe5cf19f7b7
Haibatkhan <haibatk642@gmail.com>
2025-07-17 23:21:05 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HeaderSection from './components/headersection.jsx';
-import HeroSection  from './components/herosection.jsx';
+import HeroSection from './components/herosection.jsx';
 import Aboutussection from './components/aboutussection.jsx';
 import MyWorkSection from './components/myWorkSection.jsx';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import CardDetailPage from './components/CardDetailPage'
+import CardDetailPage from './components/CardDetailPage';
 import MyServicessection from './components/myServicessection.jsx';
 import MyContactsection from './components/myContactsection.jsx';
 import MyExperiencesection from './components/myExperiencesection.jsx';
@@ -14,73 +14,26 @@ import Footersection from './components/footersection.jsx';
 function App() {
   return (
     <Router>
+      <HeaderSection />
       <Routes>
-       <Route 
-          path="/" 
-          element={
-            <div>
-              <HeaderSection />
-              <HeroSection />
-              <Aboutussection />
-              <MyWorkSection />
-              <MyServicessection />
-              <MyExperiencesection />
-              <TestiMonalsection />
-              <MyContactsection />
-              <Footersection />
-            </div>
-          } 
-        />
-
-
-        <Route 
-          path="/home" 
-          element={
-            <div>
-              <HeaderSection />
-              <HeroSection />
-            </div>
-          } 
-        />
-         
-        <Route 
-          path="/about" 
-          element={
-            <div>
-              <HeaderSection />
-              <Aboutussection />
-            </div>
-          } 
-        />
-        <Route 
-          path="/projects" 
-          element={
-            <div>
-              <HeaderSection />
-              <MyWorkSection />
-            </div>
-          } 
-        />
-        <Route 
-          path="/services" 
-          element={
-            <div>
-              <HeaderSection />
-              <MyServicessection />
-            </div>
-          } 
-        />
-        <Route 
-          path="/contacts" 
-          element={
-            <div>
-              <HeaderSection />
-              <MyContactsection />
-            </div>
-          } 
-        />
+        <Route path="/" element={
+          <>
+            <HeroSection />
+            <Aboutussection />
+            <MyWorkSection />
+            <MyServicessection />
+            <MyExperiencesection />
+            <TestiMonalsection />
+            <MyContactsection />
+          </>
+        } />
+        <Route path="/about" element={<Aboutussection />} />
+        <Route path="/projects" element={<MyWorkSection />} />
+        <Route path="/services" element={<MyServicessection />} />
+        <Route path="/contacts" element={<MyContactsection />} />
         <Route path="/details/:id" element={<CardDetailPage />} />
       </Routes>
+      <Footersection />
     </Router>
   );
 }
